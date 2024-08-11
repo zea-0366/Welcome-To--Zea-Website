fba5a04aa2d3dad2a1ccd28f1af42e8ef09820b8
farizazea9c <farizazea9c@gmail.com>
2024-08-11 16:02:51 +0000

    user name discord

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,11 +12,13 @@ function Contact() {
             <div className="footer-section">
                 <h3>Email</h3>
                 <p>farizazea9c@gmail.com</p>
+                <h3></h3>
+                <p><b>Discord: </b>FarizaZea(Usename: 066363_)</p>
+                
             </div>
             
             <div className="footer-section">
-                <h3>Social</h3>
-                <p><b>YouTube: </b>farizazeaDeasminto9c</p>
+               
             </div>
         </div>
     </div>
